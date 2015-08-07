ab97c041461b65d191a68c6775177448eb4028f4
Doug Beardsley <mightybyte@gmail.com>
2015-08-06 20:11:13 -0400

    Improve haddocks

--- src/Formattable.hs
+++ src/Formattable.hs
@@ -50,86 +50,120 @@ class Format a where
     runFormat :: TheFormat a -> a -> Text
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = NumFormat
 instance Format Integer where
     type TheFormat Integer = NumFormat
     runFormat set a = formatIntegral set a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = NumFormat
 instance Format Int where
     type TheFormat Int = NumFormat
     runFormat set a = formatIntegral set a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = NumFormat
 instance Format Int8 where
     type TheFormat Int8 = NumFormat
     runFormat set a = formatIntegral set a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = NumFormat
 instance Format Int16 where
     type TheFormat Int16 = NumFormat
     runFormat set a = formatIntegral set a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = NumFormat
 instance Format Int32 where
     type TheFormat Int32 = NumFormat
     runFormat set a = formatIntegral set a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = NumFormat
 instance Format Int64 where
     type TheFormat Int64 = NumFormat
     runFormat set a = formatIntegral set a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = NumFormat
 instance Format Word where
     type TheFormat Word = NumFormat
     runFormat set a = formatIntegral set a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = NumFormat
 instance Format Word8 where
     type TheFormat Word8 = NumFormat
     runFormat set a = formatIntegral set a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = NumFormat
 instance Format Word16 where
     type TheFormat Word16 = NumFormat
     runFormat set a = formatIntegral set a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = NumFormat
 instance Format Word32 where
     type TheFormat Word32 = NumFormat
     runFormat set a = formatIntegral set a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = NumFormat
 instance Format Word64 where
     type TheFormat Word64 = NumFormat
     runFormat set a = formatIntegral set a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = NumFormat
 instance Format Double where
     type TheFormat Double = NumFormat
     runFormat set a = formatNum set a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = NumFormat
 instance Format Float where
     type TheFormat Float = NumFormat
     runFormat set a = formatNum set a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = String
 instance Format Day where
     type TheFormat Day = String
     runFormat set a = T.pack $ formatTime defaultTimeLocale set a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = String
 instance Format UTCTime where
     type TheFormat UTCTime = String
     runFormat set a = T.pack $ formatTime defaultTimeLocale set a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = ()
 instance Format Text where
     type TheFormat Text = ()
     runFormat _ a = a
 
 
+------------------------------------------------------------------------------
+-- | TheFormat = ()
 instance Format Bool where
     type TheFormat Bool = ()
     runFormat _ a = T.pack (show a)
